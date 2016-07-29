5c9a36f4b8584927d4b4a441a81e460de627462a
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-29 16:33:04 +0200

    Pokemons fighting each other

--- src/main/java/it/morfoza/java101/pokemon/Main.java
+++ src/main/java/it/morfoza/java101/pokemon/Main.java
@@ -4,12 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Pokemon pokemon = new Pokemon();
+        Pokemon pokemon = new Pokemon("Pikachu", 100);
+        Pokemon pokemon2 = new Pokemon("Charizard", 200);
+        Pokemon pokemon3 = new Pokemon("Bulbasaur", 300);
 
-        String pokemonDescription = pokemon.toString();
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
 
-        System.out.println(pokemonDescription);
+        System.out.println();
 
+        pokemon3.attack(pokemon);
+
+        System.out.println(pokemon);
+        System.out.println(pokemon2);
+        System.out.println(pokemon3);
+
+        if(pokemon.isAlive()) {
+            System.out.println("Pokemon is alive");
+        } else {
+            System.out.println("Pokemon is unconscious");
+        }
 
     }
 }
--- src/main/java/it/morfoza/java101/pokemon/Pokemon.java
+++ src/main/java/it/morfoza/java101/pokemon/Pokemon.java
@@ -2,4 +2,34 @@ package it.morfoza.java101.pokemon;
 
 public class Pokemon {
 
+    private String name;
+    private long health = 1000;
+    private long combatPower;
+
+    public Pokemon(String name, long combatPower) {
+        this.combatPower = combatPower;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Pokemon: " + name + " health: " + health;
+    }
+
+    public void takeAttack(long attackStrength) {
+        health -= attackStrength;
+    }
+
+    public boolean isAlive() {
+        if (health > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void attack(Pokemon enemy) {
+        if (this != enemy) {
+            enemy.takeAttack(combatPower);
+        }
+    }
 }
