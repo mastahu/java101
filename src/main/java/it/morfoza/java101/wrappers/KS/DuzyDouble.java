e435160d54fe5791a22d06fc4dfad01b12b3ec8a
Karola <yumi.chan@interia.pl>
2016-08-04 15:34:02 +0200

    doubelki

---
+++ src/main/java/it/morfoza/java101/wrappers/KS/DuzyDouble.java
@@ -0,0 +1,13 @@
+package it.morfoza.java101.wrappers.KS;
+
+public class DuzyDouble {
+    private double suma;
+
+    public DuzyDouble(double suma) {
+        this.suma = suma;
+    }
+
+    public double getSuma() {
+        return suma;
+    }
+}
